513333766f5f7d74088eba9b307bd5fbdd88841a
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-02-08 14:14:11 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,6 +3,9 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
+git clone --depth 1 https://github.com/nb12nb34/luci-theme-atmaterial.git
+git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git
+git clone --depth 1 https://github.com/nb12nb34/tencentcloud-openwrt-ddns.git
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
