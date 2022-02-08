50d4fcc1a8ace0fa9fcf0031a3e7e7ac8f714d79
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-02-08 14:10:01 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,9 @@
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
+
+rm -rf ./*/.git & rm -f ./*/.gitattributes
+rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
+exit 0
