439dd871dcda6a81ced92f02010af5daa8781189
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-02-09 11:28:15 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ rm -rf $1
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
 git clone --depth 1 https://github.com/nb12nb34/luci-theme-atmaterial.git
 # git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git
-git clone --depth 1 https://github.com/nb12nb34/tencentcloud-openwrt-ddns.git
+git clone --depth 1 https://github.com/nb12nb34/luci-app-tencentddns.git
 
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-aliddns
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol
