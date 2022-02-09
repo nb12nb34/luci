56548a73c7e2b1fb2271c2459961c4b5885b5801
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-02-09 14:42:13 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,8 +7,8 @@ git clone --depth 1 https://github.com/nb12nb34/luci-theme-atmaterial.git
 # git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git
 git clone --depth 1 https://github.com/nb12nb34/luci-app-tencentddns.git
 
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-aliddns
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-aliddns
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
