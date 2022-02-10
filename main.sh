63474a82bb7dfd181db3f4142d7950063788a707
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-02-10 08:23:30 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,3 @@
-function mvdir() {
-mv -n `find $1/* -maxdepth 0 -type d` ./
-rm -rf $1
-}
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
 git clone --depth 1 https://github.com/nb12nb34/luci-theme-atmaterial.git
 # git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git
@@ -9,7 +5,3 @@ git clone --depth 1 https://github.com/nb12nb34/luci-app-tencentddns.git
 
 svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol
-
-rm -rf ./*/.git & rm -f ./*/.gitattributes
-rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
-exit 0
