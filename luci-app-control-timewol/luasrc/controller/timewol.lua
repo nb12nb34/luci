5507dd070878f155e6f89753e967f24b121edcec
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-02-12 14:55:51 +0800

    Update timewol.lua

--- luci-app-control-timewol/luasrc/controller/timewol.lua
+++ luci-app-control-timewol/luasrc/controller/timewol.lua
@@ -3,7 +3,7 @@ module("luci.controller.timewol", package.seeall)
 function index()
     if not nixio.fs.access("/etc/config/timewol") then return end
 
-    entry({"admin", "services", "timewol"}, cbi("timewol"), _("定时唤醒"), 91).dependent = true
+    entry({"admin", "services", "timewol"}, cbi("timewol"), _("Timewol"), 91).dependent = true
 end
 
 
