40e63000ea862ecacaa19ee9d91905e1e9d2a976
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-02-17 11:17:51 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,6 +10,8 @@ git clone --depth 1 https://github.com/nb12nb34/luci-app-tencentddns.git
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-aliddns
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol
 svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman
+svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
+
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
