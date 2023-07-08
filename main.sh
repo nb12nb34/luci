ce90016d11336b618c39d8673f1df787c377769e
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2023-07-08 08:12:18 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
-git clone --depth 1 https://github.com/nb12nb34/luci-theme-atmaterial.git
+# git clone --depth 1 https://github.com/nb12nb34/luci-theme-atmaterial.git
 # git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git
 git clone --depth 1 https://github.com/nb12nb34/luci-app-tencentddns.git
 
