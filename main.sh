df3b9ac904e7816943c000627d1ca52bf09a3c03
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2026-02-09 09:05:08 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,10 +7,11 @@ git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
 # git clone --depth 1 https://github.com/tty228/luci-app-serverchan.git
 git clone --depth 1 https://github.com/nb12nb34/luci-app-tencentddns.git
 
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-aliddns
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-aliddns
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-control-timewol
-svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman
-svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
+# svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman
+# svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
+svn co https://github.com/coolsnowwolf/luci/tree/master/applications/luci-app-samba4
 
 
 # rm -rf ./*/.git & rm -f ./*/.gitattributes
